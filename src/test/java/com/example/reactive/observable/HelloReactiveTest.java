43bbfe13c0c7e0b800520db249cdbe2fedc98974
sruizvass <sergio.ruiz@vass.es>
2022-04-10 15:41:23 +0200

    commit test observable

--- src/test/java/com/example/reactive/observable/HelloReactiveTest.java
+++ src/test/java/com/example/reactive/observable/HelloReactiveTest.java
@@ -3,6 +3,8 @@ package com.example.reactive.observable;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,5 +20,19 @@ class HelloReactiveTest {
         Observable<String> observer = Observable.just("Hello"); // provides datea
         observer.subscribe(s -> result=s); // Callable as subscriber
         assertEquals("Hello",result);
+    }
+
+    @Test
+    void suscribeToObservable(){
+        Observable<String> observable = Observable.fromArray("hola","que","tal");
+        observable.subscribe(text -> System.out.println(text));
+    }
+
+    @Test
+    void givenRangeOfNumberWhenMultiplyThenNumberOK(){
+        Observable.range(1,10)
+                .map(x->x*10)
+                .subscribe(System.out::println);
+
     }
 }
