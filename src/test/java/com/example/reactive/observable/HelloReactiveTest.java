940ec53025562422ef98732f306b6e4fd6b6ce34
sruizvass <sergio.ruiz@vass.es>
2022-04-10 21:09:23 +0200

    test merge and zip operators

--- src/test/java/com/example/reactive/observable/HelloReactiveTest.java
+++ src/test/java/com/example/reactive/observable/HelloReactiveTest.java
@@ -2,9 +2,15 @@ package com.example.reactive.observable;
 
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subscribers.TestSubscriber;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 
 class HelloReactiveTest {
@@ -32,5 +38,32 @@ class HelloReactiveTest {
                 .map(x->x*10)
                 .subscribe(System.out::println);
 
+    }
+
+    @Test
+    void givenTwoObservables_whenMerged_shouldEmitCombinedResults() {
+        List<String> mergeStrings = new ArrayList<>();
+
+        Observable.merge(
+                Observable.fromArray(new String[] { "Simple", "Moderate", "Complex" }),
+                Observable.fromArray(new String[] { "Solutions", "Success", "Hierarchy"})
+        ).subscribe(mergeStrings::add);
+
+        mergeStrings.forEach(System.out::println);
+    }
+
+    @Test
+    void givenTwoObservables_whenZipped_thenReturnCombinedResults() {
+        List<String> zippedStrings = new ArrayList<>();
+
+        Observable.zip(
+                Observable.fromArray(new String[] { "Simple", "Moderate", "Complex" }),
+                Observable.fromArray(new String[] { "Solutions", "Success", "Hierarchy"}),
+                (str1, str2) -> str1 + " " + str2).subscribe(zippedStrings::add);
+
+        zippedStrings.forEach(System.out::println);
+
+        assertEquals(false,zippedStrings.isEmpty());
+        assertEquals(3,zippedStrings.size());
     }
 }
